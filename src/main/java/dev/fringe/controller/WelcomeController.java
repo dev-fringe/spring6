5a2c4fb78742d4661e2614eeaa18808bfa99af7e
hdlee <dev.fringe@gmail.com>
2022-12-10 23:45:05 +0900

    jakarta

--- src/main/java/dev/fringe/controller/WelcomeController.java
+++ src/main/java/dev/fringe/controller/WelcomeController.java
@@ -2,6 +2,8 @@ package dev.fringe.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
@@ -21,8 +24,9 @@ import lombok.extern.log4j.Log4j2;
 public class WelcomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public  String home() {
+	public  String home(Model model) {
 		log.debug("{}", () -> "Controller");
+		model.addAttribute("message", new Date());
 		return "Hello";
 	}
 
