ef5a0fe293a4b49ed2e09a0ec57988f9dc81b670
hdlee <dev.fringe@gmail.com>
2022-12-10 22:32:05 +0900

    init

---
+++ src/main/java/dev/fringe/config/CommonConfig.java
@@ -0,0 +1,9 @@
+package dev.fringe.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableWebMvc
+//@ComponentScan("dev.fringe.controller")
+public class CommonConfig {
+}
