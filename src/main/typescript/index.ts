3251b76f9852c40ee9edcef280bcddc2af540d0b
hdlee <dev.fringe@gmail.com>
2022-12-16 00:05:55 +0900

    init

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,26 +1,26 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 import { customElement, property } from 'lit-element/decorators.js';
- 
+import { styles } from './styles';
+
 @customElement('fetching-data')
 export class FetchingData extends LitElement {
 
-  @property({type : Array})
-  res = []
+	@property({ type: Array })
+	res = []
+ 
+	firstUpdated() {
+		fetch("/get.do").then((r) => r.json()).then((r) => {
+			this.res = r.results
+		})
+	}
 
-  firstUpdated() {
-    fetch("/get.do")
-      .then((r) => r.json())
-      .then((r) => {
-        this.res = r.results
-      })
-  }
-
-  render() {
-    const { res } = this;
-    return html`
-      <ul>
-        ${res.map((item) => html` <li>${item.name}</li> `)}
-      </ul>
-    `
-  }
+	render() {
+	const { res } = this;
+		return html`
+			${styles}
+			<ul>
+				${res.map((item) => html` <li class="message">${item.name}</li> `)}
+			</ul>
+		`
+	}
 }
---
+++ src/main/typescript/styles.ts
@@ -0,0 +1,13 @@
+import { html } from 'lit-element';
+
+// In this file we define our css inside a style tag and export it.
+export const styles = html`
+  <style>
+    :host {
+        display: block;
+    }
+    .message {
+        color: blue;
+    }
+  </style>
+`;
