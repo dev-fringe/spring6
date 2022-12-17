f561ca224dfee2c1afb72d37e77742b8ca5a09b1
hdlee <dev.fringe@gmail.com>
2022-12-17 22:23:45 +0900

    init

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,34 +1,40 @@
 import { LitElement, html } from 'lit'
 import { customElement, property } from 'lit-element/decorators.js'
 import { repeat } from 'lit-html/directives/repeat.js'
+import { styles } from './styles'
 
 @customElement('fetching-data')
 export class FetchingData extends LitElement {
 
 	@property()
 	res = []
-	value = "" 
- 
-   constructor() {
-    super();
-	}
-  
+	value = ""
+
 	render() {
 		return html`
+		${styles}
 		<input type="text"  name="line-1" .value="${this.value}" @input="${this._setValue}">
-		<button @click="${this.click}">get</button>
-		<ul>
-		${repeat(this.res,item => html` <li class="message">${item.name}</li> `)}
-		</ul>
+		<button @click="${this._click}">get</button>
+		<table>
+			<thead>
+				<tr>
+					<th>
+						Name
+					</th> 
+				</tr>
+			</thead>
+			<tbody>
+				${repeat(this.res,item => html`<tr class="striped"><td>${item.name}</td></tr>`)}
+			<tbody>
+		</table>
 		`
 	}
-	click() {
-		console.log(this.value);
+	_click() {
 		fetch("/get.do?value=" + this.value).then((r) => r.json()).then((r) => {
 			this.res = r.results
 		})		 
 	}
 	_setValue(e) { 
-		this.value = e.target.value;
+		this.value = e.target.value
 	}
 } 
